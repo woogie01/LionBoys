05ae1d380f10cfd9b52f1a1e169d13891c9b852b
woogie01 <kwonhu3973@gmail.com>
2025-11-10 20:18:25 +0900

    [Chore] : Bootstrap Spring Boot Project

---
+++ src/main/java/likelion/lionboys/LionBoysApplication.java
@@ -0,0 +1,13 @@
+package likelion.lionboys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionBoysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LionBoysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/likelion/lionboys/LionBoysApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion.lionboys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LionBoysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
