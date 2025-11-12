01b79484f2cb43389cbd9e1dad5395adce384207
KKT <kimgt2828@gmail.com>
2025-11-12 16:50:08 +0900

    [Feat/#1] : S3 Config 추가
    - accessKey sercetKey, region 등으로 credential 추가 후 S3Client 생성

---
+++ src/main/java/likelion/lionboys/global/config/S3Config.java
@@ -0,0 +1,38 @@
+package likelion.lionboys.global.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    public AmazonS3 amazonS3() {
+
+        // AWS 자격 증명 등록
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        // S3 Client 생성 및 Bean 등록
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
+                .build();
+
+        return s3Client;
+    }
+}
