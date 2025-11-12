da67209908fd8543c424aa31f79566b996aa2063
KKT <kimgt2828@gmail.com>
2025-11-12 14:35:51 +0900

    [Refact/#1] : Indexing 처리 및 검증 및 상태 변경 함수 추가 - Image.java: 업로드 상태 검증 로직, s3Key 컬럼 인덱싱 추가 - ImageService: 이미지 업로드 서비스 클래스

--- src/main/java/likelion/lionboys/domain/image/entity/Image.java
+++ src/main/java/likelion/lionboys/domain/image/entity/Image.java
@@ -2,8 +2,11 @@ package likelion.lionboys.domain.image.entity;
 
 import jakarta.persistence.*;
 import likelion.lionboys.domain.common.BaseTimeEntity;
+import likelion.lionboys.domain.image.exception.ImageException;
+import likelion.lionboys.domain.image.exception.error.ImageErrorCode;
 
-@Table(name = "images")
+@Table(name = "images",
+indexes = @Index(name = "idx_s3_key", columnList = "s3key"))
 @Entity
 public class Image extends BaseTimeEntity {
 
@@ -14,6 +17,10 @@ public class Image extends BaseTimeEntity {
     @Column(nullable = false, unique = true, length = 500)
     private String s3Key;
 
+    @Column(nullable = false)
+    private Long fileSize;
+
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
     private UploadStatus uploadStatus;
@@ -21,4 +28,31 @@ public class Image extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     @Column(length = 20)
     private ContentType contentType;  // DB에 'JPEG', 'PNG' 저장됨
+
+    /*
+    업로드 처리 완료
+     */
+    public void completeUpload(Long fileSize) {
+        if(this.uploadStatus == UploadStatus.PENDING) {
+            throw new ImageException(ImageErrorCode.IMAGE_NOT_FOUND, "PENDING 상태에서만 완료 처리할 수 있습니다.");
+        }
+        this.uploadStatus = UploadStatus.COMPLETED;
+        // 등록 시각 변경하기
+        this.fileSize = fileSize;
+    }
+
+    /*
+    업로드 처리 실패
+     */
+    public void failUpload() {
+        this.uploadStatus = UploadStatus.FAILED;
+    }
+
+    /*
+    업로드 완료된 이미지인지 확인
+     */
+    public boolean isCompleted() {
+        return this.uploadStatus == UploadStatus.COMPLETED;
+    }
+
 }
---
+++ src/main/java/likelion/lionboys/domain/image/service/ImageService.java
@@ -0,0 +1,11 @@
+package likelion.lionboys.domain.image.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class ImageService {
+
+
+}
